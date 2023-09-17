37526213bc0ebb89853f6eb6f8e66c45a814d294
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2023-09-17 19:09:26 +0700

    Add main gameplay of rock paper scissors

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
+    const choice = ['rock', 'paper', 'scissors'];
     const randomChoice = choice[Math.floor(Math.random() * choice.length)];
     return randomChoice;
 }
 
-console.log(getComputerChoice());
+let playerSelection = 'Rock';
+let computerSelection = getComputerChoice();
+
+playerSelection = playerSelection.toLowerCase();
+
+function playRound (playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return "Draw!";
+    } else if(playerSelection == 'rock') {
+        return (computerSelection == 'scissors') ? "You win!" : "You lose!";
+    } else if(playerSelection == 'paper') {
+        return (computerSelection == 'rock') ? "You win!" : "You lose!";
+    } else if(playerSelection == 'scissors') {
+        return (computerSelection == 'paper') ? "You win!" : "You lose!";
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
